9711027711dbb59e3d594177885278977087dd89
Filigrani <filigranisb@gmail.com>
2017-07-14 14:23:50 +0300

    Anim Demo

---
+++ anim_demo/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1500031321,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player_player_boot_2-sheet0.png",
+		"images/player_player_hand_000-sheet0.png",
+		"images/player_player_boot_1-sheet0.png",
+		"images/player_player_shield-sheet0.png",
+		"images/player_player_body-sheet0.png",
+		"images/player_player_head_4-sheet0.png",
+		"images/player_player_head_4-sheet1.png",
+		"images/player_player_head_4-sheet2.png",
+		"images/player_player_hair-sheet0.png",
+		"images/player-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"player.scml",
+		"player.scon"
+	]
+}
